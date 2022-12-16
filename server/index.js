92114c187861e4c7c92f42d69a3e3dc121de0490
Harshit Rathi <harshitr2001@gmail.com>
2022-12-16 16:07:06 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use("/", require('./routes/user'));
 app.use("/", require('./routes/course'));
 
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log("Done");
